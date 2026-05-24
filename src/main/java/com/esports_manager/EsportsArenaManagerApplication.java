ec5204dae94f364a99715b0842b37849baab6f36
jhon olivares <jh.olivaresi@duocuc.cl>
2026-05-24 18:16:38 -0400

    first commit

---
+++ src/main/java/com/esports_manager/EsportsArenaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.esports_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsArenaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsArenaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esports_manager/EsportsArenaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esports_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsArenaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
